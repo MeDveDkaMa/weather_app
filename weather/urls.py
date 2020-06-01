ce6ac2090c5837caa7588d6aba7acf092c09e665
Alexander Biryulyov <medvedkama@gmail.com>
2020-06-02 00:14:04 +0300

    Add class AddCity

--- weather/urls.py
+++ weather/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CurrentTemperatureView.as_view()),
+    path('', views.CurrentTemperatureView.as_view(), name="current_temp"),
+    path('api/add/', views.AddCity.as_view(), name="add_city"),
     # path('api/forecast', views.forecast),
     # path('api/test', views.get_temperature_history)
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .models import City, Information, History
+from .forms import AddCityForm
 
 
-class CurrentTemperatureView(TemplateView):
+class BaseView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["information_list"] = Information.objects.all()
+        context["city_list"] = City.objects.all()
+        context["form"] = AddCityForm
+        return context
+
+
+class CurrentTemperatureView(BaseView):
     template_name = 'weather/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["information"] = Information.objects.all()
-        context["city"] = City.objects.all()
+        # context["information_list"] = Information.objects.all()
+        # context["city_list"] = City.objects.all()
         return context
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        print(context)
+        return self.render_to_response(context)
+
+
+class AddCity(BaseView):
+
+    def post(self, request, *args, **kwargs):
+        form = AddCityForm(request.POST)
+        if form.is_valid():
+            print("valid")
+            form.save()
+        # context = self.get_context_data(**kwargs)
+        # print(context)
+        # print(request.POST)
+        return redirect("/")
