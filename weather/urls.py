f4582659213d88375d704f87688688635a8dbccd
Alexander Biryulyov <medvedkama@gmail.com>
2020-06-03 16:25:51 +0300

    fix

--- weather/urls.py
+++ weather/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.CurrentTemperatureView.as_view(), name="current_temp"),
-    path('api/add/', views.AddCity.as_view(), name="add_city"),
+    path('api/add/', views.AddCityView.as_view(), name="add_city"),
+    # path('api/info/<int:pk>/', views.InformationCityView.as_view(), name="information_city"),
+    path('api/info/', views.InformationCityView.as_view(), name="information_city"),
+    path('api/addinfo/', views.AddInformationView.as_view(), name="add_info"),
+    # path('api/add/<slug:slug>/', views.AddCity.as_view(), name="add_city"),
     # path('api/forecast', views.forecast),
     # path('api/test', views.get_temperature_history)
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,8 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .models import City, Information, History
-from .forms import AddCityForm
+from .forms import AddCityForm, AddInformationForm
 import requests
 
 
@@ -11,6 +12,7 @@ class BaseView(TemplateView):
         context["information_list"] = Information.objects.all()
         context["city_list"] = City.objects.all()
         context["form"] = AddCityForm
+        context["info_form"] = AddInformationForm
         return context
 
 
@@ -19,48 +21,34 @@ class CurrentTemperatureView(BaseView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context["information_list"] = Information.objects.all()
-        # context["city_list"] = City.objects.all()
+        print(context)
         return context
 
-    def post(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        print(context)
 
-        return self.render_to_response(context)
-
-
-class AddCity(BaseView):
+class AddCityView(BaseView):
+    template_name = 'weather/index.html'
 
     def post(self, request, *args, **kwargs):
-
         form = AddCityForm(request.POST)
         if form.is_valid():
-            print("valid")
             form.save()
+        return redirect("information_city")
 
 
-        token = 'ca8ee28f8bf42eb6948dba8bcc7aa661'
-        url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + token
+class InformationCityView(BaseView):
+    template_name = 'weather/cityInformation.html'
 
-        cities = City.objects.all()
-        for city in cities:
-            res = requests.get(url.format(city.name)).json()
-            city_info = {
-                'city': city.name,
-                'temp': res["main"]["temp"],
-                'icon': res["weather"][0]["icon"],
-                'sky': res["weather"][0]["description"],
-                'coord_lon': res["coord"]["lon"],
-                'coord_lat': res["coord"]["lat"],
-            }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        return context
 
-            Information.objects.filter(city__name=city_info['city']).update(temperature=city_info['temp'])
-            Information.objects.filter(city__name=city_info['city']).update(sky=city_info['sky'])
-            Information.objects.filter(city__name=city_info['city']).update(coord_lon=city_info['coord_lon'])
-            Information.objects.filter(city__name=city_info['city']).update(coord_lat=city_info['coord_lat'])
 
-        # context = self.get_context_data(**kwargs)
-        # print(context)
-        print(request.POST)
-        return redirect("/")
+class AddInformationView(BaseView):
+    template_name = 'weather/index.html'
+
+    def post(self, request, *args, **kwargs):
+        form = AddInformationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("information_city")
