daf58661f5fd68b2c1880bd96e954d741b5e6b6e
Alexander Biryulyov <medvedkama@gmail.com>
2020-06-03 18:40:54 +0300

    add correct database insert

--- weather/views.py
+++ weather/views.py
@@ -28,10 +28,49 @@ class CurrentTemperatureView(BaseView):
 class AddCityView(BaseView):
     template_name = 'weather/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        return context
+
     def post(self, request, *args, **kwargs):
+        token = 'ca8ee28f8bf42eb6948dba8bcc7aa661'
+        url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + token
+
+        context = super().get_context_data(**kwargs)
         form = AddCityForm(request.POST)
         if form.is_valid():
             form.save()
+
+        info_form_data = form.cleaned_data
+
+        print(info_form_data)
+
+        try:
+            info_city = City.objects.get(name=info_form_data["name"])
+            print("Найдено")
+        except:
+            raise Http404
+
+        cities = City.objects.all()
+        for city in cities:
+            res = requests.get(url.format(city.name)).json()
+            city_info = {
+                'city': city.name,
+                'temp': res["main"]["temp"],
+                'icon': res["weather"][0]["icon"],
+                'sky': res["weather"][0]["description"],
+                'coord_lon': res["coord"]["lon"],
+                'coord_lat': res["coord"]["lat"],
+            }
+
+            Information.objects.update_or_create(city_id=info_city.id)
+            Information.objects.filter(city_id=info_city.id).update(temperature=city_info['temp'])
+            Information.objects.filter(city_id=info_city.id).update(coord_lon=city_info['coord_lon'])
+            Information.objects.filter(city_id=info_city.id).update(coord_lat=city_info['coord_lat'])
+            Information.objects.filter(city_id=info_city.id).update(sky=city_info['sky'])
+            Information.objects.filter(city_id=info_city.id).update(icon=city_info['icon'])
+
         return redirect("information_city")
 
 
@@ -47,8 +86,17 @@ class InformationCityView(BaseView):
 class AddInformationView(BaseView):
     template_name = 'weather/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # print("GETCONTEXTINFO")
+        return context
+
+
     def post(self, request, *args, **kwargs):
         form = AddInformationForm(request.POST)
+        print(request.POST)
         if form.is_valid():
+            form = form.save(commit=False)
+            form.city_id = 141
             form.save()
-        return redirect("information_city")
+        return redirect("/")
