339ee384f2376714f01e0c0ed58795716443eda8
Alexander Biryulyov <medvedkama@gmail.com>
2020-06-05 01:58:54 +0300

    Update views for new fields

--- weather/views.py
+++ weather/views.py
@@ -61,22 +61,38 @@ class AddCityView(BaseView):
                 'sky': res["weather"][0]["description"],
                 'icon': res["weather"][0]["icon"],
                 'temp': res["main"]["temp"],
-                'pressure': res["main"]["pressure"],
-                'humidity': res["main"]["humidity"],
+                'temp_feels': res["main"]["feels_like"],
                 'temp_min': res["main"]["temp_min"],
                 'temp_max': res["main"]["temp_max"],
+                'pressure': res["main"]["pressure"],
+                'humidity': res["main"]["humidity"],
                 'visibility': res["visibility"],
                 'speed': res["wind"]["speed"],
-                'city': city.name,
-
+                'time': res["dt"],
+                'country': res["sys"]["country"],
+                'sunrise': res["sys"]["sunrise"],
+                'sunset': res["sys"]["sunset"],
+                'city': city.name
             }
 
             Information.objects.update_or_create(city_id=info_city.id)
-            Information.objects.filter(city_id=info_city.id).update(temperature=city_info['temp'])
             Information.objects.filter(city_id=info_city.id).update(coord_lon=city_info['coord_lon'])
             Information.objects.filter(city_id=info_city.id).update(coord_lat=city_info['coord_lat'])
             Information.objects.filter(city_id=info_city.id).update(sky=city_info['sky'])
             Information.objects.filter(city_id=info_city.id).update(icon=city_info['icon'])
+            Information.objects.filter(city_id=info_city.id).update(temperature=city_info['temp'])
+            Information.objects.filter(city_id=info_city.id).update(feels_like=city_info['temp_feels'])
+            Information.objects.filter(city_id=info_city.id).update(temp_min=city_info['temp_min'])
+            Information.objects.filter(city_id=info_city.id).update(temp_max=city_info['temp_max'])
+            Information.objects.filter(city_id=info_city.id).update(pressure=city_info['pressure'])
+            Information.objects.filter(city_id=info_city.id).update(humidity=city_info['humidity'])
+            Information.objects.filter(city_id=info_city.id).update(visibility=city_info['visibility'])
+            Information.objects.filter(city_id=info_city.id).update(speed=city_info['speed'])
+            Information.objects.filter(city_id=info_city.id).update(time=city_info['time'])
+            Information.objects.filter(city_id=info_city.id).update(country=city_info['country'])
+            Information.objects.filter(city_id=info_city.id).update(sunrise=city_info['sunrise'])
+            Information.objects.filter(city_id=info_city.id).update(sunset=city_info['sunset'])
+
         return redirect("/")
         # return self.render_to_response(context)
 
@@ -116,20 +132,42 @@ class UpdateInformationView(BaseView):
         for city in cities:
             res = requests.get(url.format(city.name)).json()
             city_info = {
-                'city': city.name,
-                'temp': res["main"]["temp"],
-                'icon': res["weather"][0]["icon"],
-                'sky': res["weather"][0]["description"],
                 'coord_lon': res["coord"]["lon"],
                 'coord_lat': res["coord"]["lat"],
+                'sky': res["weather"][0]["description"],
+                'icon': res["weather"][0]["icon"],
+                'temp': res["main"]["temp"],
+                'temp_feels': res["main"]["feels_like"],
+                'temp_min': res["main"]["temp_min"],
+                'temp_max': res["main"]["temp_max"],
+                'pressure': res["main"]["pressure"],
+                'humidity': res["main"]["humidity"],
+                'visibility': res["visibility"],
+                'speed': res["wind"]["speed"],
+                'time': res["dt"],
+                'country': res["sys"]["country"],
+                'sunrise': res["sys"]["sunrise"],
+                'sunset': res["sys"]["sunset"],
+                'city': city.name,
             }
             print("API RESPONSE:", city_info)
-            Information.objects.filter(city_id=city.id).update(temperature=city_info['temp'])
+
             Information.objects.filter(city_id=city.id).update(coord_lon=city_info['coord_lon'])
             Information.objects.filter(city_id=city.id).update(coord_lat=city_info['coord_lat'])
             Information.objects.filter(city_id=city.id).update(sky=city_info['sky'])
             Information.objects.filter(city_id=city.id).update(icon=city_info['icon'])
-
+            Information.objects.filter(city_id=city.id).update(temperature=city_info['temp'])
+            Information.objects.filter(city_id=city.id).update(feels_like=city_info['temp_feels'])
+            Information.objects.filter(city_id=city.id).update(temp_min=city_info['temp_min'])
+            Information.objects.filter(city_id=city.id).update(temp_max=city_info['temp_max'])
+            Information.objects.filter(city_id=city.id).update(pressure=city_info['pressure'])
+            Information.objects.filter(city_id=city.id).update(humidity=city_info['humidity'])
+            Information.objects.filter(city_id=city.id).update(visibility=city_info['visibility'])
+            Information.objects.filter(city_id=city.id).update(speed=city_info['speed'])
+            Information.objects.filter(city_id=city.id).update(time=city_info['time'])
+            Information.objects.filter(city_id=city.id).update(country=city_info['country'])
+            Information.objects.filter(city_id=city.id).update(sunrise=city_info['sunrise'])
+            Information.objects.filter(city_id=city.id).update(sunset=city_info['sunset'])
         return redirect("/")
 
 
