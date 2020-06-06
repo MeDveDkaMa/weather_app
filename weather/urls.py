99013c72c5222da4d3ac6593994a888de7e76ce2
Alexander Biryulyov <medvedkama@gmail.com>
2020-06-06 17:53:47 +0300

    Add more params for history page

--- weather/urls.py
+++ weather/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.CurrentTemperatureView.as_view(), name="current_temp"),
     path('api/add/', views.AddCityView.as_view(), name="add_city"),
     path('api/info/<int:pk>/', views.InformationCityView.as_view(), name="information_city"),
-    path('api/history/<int:pk>/', views.CityTemperatureHistory.as_view(), name="history_city"),
+    path('api/history/<int:pk>/time/<int:time>/', views.CityTemperatureHistory.as_view(), name="history_city"),
     path('api/info/update/', views.UpdateInformationView.as_view(), name="update_info"),
     path('api/info/temperature/all', views.AllCityTemperatureView.as_view(), name="all_temperature_info"),
 ]
