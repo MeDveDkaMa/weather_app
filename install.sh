4d79a916fca910fb62240e0c3b7226ec1597fe71
Alexander Biryulyov <medvedkama@gmail.com>
2020-06-07 17:51:45 +0300

    add install script for first setup. Update start.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install python3.7
+pip3.7 install virtualenv
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pwd
-pip3.7 install virtualenv
 python3.7 -m virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
